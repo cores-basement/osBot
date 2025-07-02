344b7aa0ae3bf98eac5b57068a0ad6ea738b576b
anuj-weebjs <weeb.js@yahoo.com>
2025-07-02 09:28:56 +0530

    added crash command to make it crash

---
+++ src/commands/danger/crash/crash.ts
@@ -0,0 +1,23 @@
+import { Client, Message } from "discord.js";
+
+var config = require('../../../../config.json');
+var developerId = config.developerId;
+module.exports = {
+    structure:{
+        name:"crash",
+        description: "only Developer can use this",
+        usage:".."
+    },
+    execute: async(message: Message, client: Client)=>{
+        if(!message.channel.isSendable()) return;
+        if(message.author.id == developerId){
+            await message.channel.send("Crashed");
+            await client.destroy();
+            process.exit(1);
+        }else{
+             message.channel.send("https://tenor.com/view/aww-hell-nahh-aww-hell-aww-hell-nahh-gif-9659330954843262630")
+            return;
+        }
+    },
+
+};
